e4ad007d40647106ad8ea458988c60be38d7d5d8
viylouu <131220600+viylouu@users.noreply.github.com>
2026-04-03 23:46:17 -0600

    goodbye root.zig boo

--- core/root.zig
+++
@@ -1,6 +0,0 @@
-//const ear_ll_vk_util_buffer = @import("ear/ll/vk/util/buffer.zig");
-//
-//comptime{
-//    _ = ear_ll_vk_util_buffer._ear_vk_find_memory_type;
-//    _ = ear_ll_vk_util_buffer._ear_vk_make_buf;
-//}
